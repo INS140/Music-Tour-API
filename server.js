e11710a374f58a2b47c65fcf8b22b80fa12c93b8
INS140 <IsaacNSaettel@gmail.com>
2023-04-06 21:00:07 -0700

    setup model and config

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate()
+    console.log('Database connected on port 5432')
+} catch(err) {
+    console.log(err)
+}
 
 // ROOT
 app.get('/', (req, res) => {
